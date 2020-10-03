e8570ef0d6901bba7462ab8f1aa00bd2b6c365a8
Yurii Konoval <yuriioleksandrovichkonoval@gmail.com>
2020-10-03 23:35:15 +0300

    исправления

--- js/task-8.js
+++ js/task-8.js
@@ -3,10 +3,6 @@ const boxesEl = document.querySelector(`#boxes`);
 const renderEl = document.querySelector(`[data-action="render"]`);
 const destroyEl = document.querySelector(`[data-action="destroy"]`);
 
-let boxNumber = 0;
-let elements = [];
-
-inputEl.addEventListener(`input`, userChangeNumber);
 renderEl.addEventListener(`click`, render);
 destroyEl.addEventListener(`click`, destroyBoxes);
 
@@ -26,23 +22,33 @@ function randomColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-function userChangeNumber(event) {
-  return (boxNumber = event.currentTarget.value);
+function render(event) {
+  const boxNumber = inputEl.value;
+  createBoxes(boxNumber);
 }
 
+// function createBoxes(amount) {
+//   let elements = [];
+//   for (let i = 0; i < amount; i += 1) {
+//     const size = 30 + i * 10;
+//     const div = createBox(size);
+//     elements.push(div);
+//   }
+//   boxesEl.append(...elements);
+//   elements = [];
+// }
+
 function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+
   for (let i = 0; i < amount; i += 1) {
     const size = 30 + i * 10;
     const div = createBox(size);
-    elements.push(div);
+    fragment.appendChild(div);
   }
-  boxesEl.append(...elements);
-  elements = [];
-  return boxesEl;
+  console.log(fragment);
+  boxesEl.append(fragment);
 }
 function destroyBoxes() {
   return (boxesEl.innerHTML = ``);
-}
-function render() {
-  createBoxes(boxNumber);
 }
